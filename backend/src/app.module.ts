30af0a672c74ab3381bb2aba052c9a66baa2d18c
Wangprai <wangprai.jullapech@gmail.com>
2026-08-05 00:42:16 +0700

    fix: add WorkspaceMembers to app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { WorkspacesModule } from './modules/workspaces/workspaces.module';
+import { WorkspaceMembersModule } from './modules/workspace-members/workspace-members.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, WorkspacesModule],
+  imports: [PrismaModule, AuthModule, UsersModule, WorkspacesModule, WorkspaceMembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
